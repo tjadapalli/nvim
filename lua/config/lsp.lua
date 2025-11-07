34056214943b83d6fd16a1acf46bbf9e648246d8
Tharshith Jadapalli <contact@tharshith.com>
2025-11-07 01:13:21 -0800

    Ignore: Tried to add harper_ls

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -27,4 +27,38 @@ vim.diagnostic.config({
 	severity_sort = true,     -- sort diagnostics by severity
 })
 
+--[[ vim.lsp.config('harper_ls', {
+ settings = {
+    ["harper-ls"] = {
+      userDictPath = "",
+      workspaceDictPath = "",
+      fileDictPath = "",
+      linters = {
+        SpellCheck = true,
+        SpelledNumbers = false,
+        AnA = true,
+        SentenceCapitalization = true,
+        UnclosedQuotes = true,
+        WrongQuotes = false,
+        LongSentences = true,
+        RepeatedWords = true,
+        Spaces = true,
+        Matcher = true,
+        CorrectNumberSuffix = true
+      },
+      codeActions = {
+        ForceStable = false
+      },
+      markdown = {
+        IgnoreLinkTitle = false
+      },
+      diagnosticSeverity = "hint",
+      isolateEnglish = false,
+      dialect = "American",
+      maxFileLength = 120000,
+      ignoredLintsPath = "",
+      excludePatterns = {}
+    }
+  }
+}) --]]
 
